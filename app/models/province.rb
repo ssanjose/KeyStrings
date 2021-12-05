31ff077cb2125b4eb39b6780ead704b0ef2917ac
ssanjose <ssanjose@academic.rrc.ca>
2021-12-05 01:45:20 -0600

    Added validation to province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,6 @@
 class Province < ApplicationRecord
+  has_many :customer
+  validates :title, :pst, :gst, presence: true
+  validates :title, uniqueness: true
+  validates :pst, :gst, numericality: true
 end
