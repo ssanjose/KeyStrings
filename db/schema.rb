7a061fd584021f1ba2631f96d6edc710aa2c8cdb
ssanjose <ssanjose@academic.rrc.ca>
2021-12-05 01:47:31 -0600

    Added category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_074537) do
+ActiveRecord::Schema.define(version: 2021_12_05_074655) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "provinces", force: :cascade do |t|
     t.string "title"
