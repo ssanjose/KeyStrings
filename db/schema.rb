5f497d61c56cadf12b6b277616f8ebf88883195a
ssanjose <ssanjose@academic.rrc.ca>
2021-12-05 02:26:27 -0600

    Added devise to users, for real

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_074655) do
+ActiveRecord::Schema.define(version: 2021_12_05_082159) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -28,13 +28,19 @@ ActiveRecord::Schema.define(version: 2021_12_05_074655) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.text "password"
     t.integer "phone"
     t.integer "province_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["province_id"], name: "index_users_on_province_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "users", "provinces"
