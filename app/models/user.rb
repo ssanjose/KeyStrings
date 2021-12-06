252015fae50b9f20c8eb6e7cd5a1ed631b6ffef8
ssanjose <ssanjose@academic.rrc.ca>
2021-12-05 19:35:50 -0600

    Added custom user registration and log in pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   belongs_to :province
-  validates :name, :email, :password, presence: true
+  validates :email, presence: true,
+                    format:   { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Must be a valid email address" }
+  validates :name, :password, presence: true
   validates :email, uniqueness: true
-  validates :phone, numericality: { only_integer: true }
+  validates :phone, numericality: true, length: { minimum: 10, maximum: 15 }
 end
