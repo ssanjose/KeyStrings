6ca2533b49d841ef5bd465fdb7cc7c53a4a89c81
ssanjose <ssanjose@academic.rrc.ca>
2021-12-08 13:51:14 -0600

    Edited admin page for models

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,11 +1,20 @@
 ActiveAdmin.register Item do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :description, :price, :category_id
+  permit_params :title, :description, :price, :category_id, :picture
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
   #
   # or
   #
@@ -14,5 +23,4 @@ ActiveAdmin.register Item do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Province do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :pst, :gst
+  permit_params :title, :pst, :gst
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Province do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
