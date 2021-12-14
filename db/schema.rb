553fd7a6cdd98ded173035d84c8158a408f7b0e2
ssanjose <ssanjose@academic.rrc.ca>
2021-12-14 10:50:36 -0600

    Changes added taxes to pst and gst

--- db/schema.rb
+++ db/schema.rb
@@ -110,7 +110,8 @@ ActiveRecord::Schema.define(version: 2021_12_14_155627) do
     t.integer "discount_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "taxes", null: false
+    t.decimal "pst", null: false
+    t.decimal "gst", null: false
     t.index ["discount_id"], name: "index_orders_on_discount_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
